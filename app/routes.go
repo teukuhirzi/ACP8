5c66a51f4298e13cd70d3e4145f54fd67c09500f
teukuhirzi <lowfinecity@gmail.com>
2021-06-02 20:07:16 +0700

    recommit

--- app/routes.go
+++ app/routes.go
@@ -4,5 +4,5 @@ import "github.com/teukuhirzi/altastore/app/controllers"
 
 func (server *Server) initializeRoutes() {
 	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
-	server.Router.HandleFunc()
+	//server.Router.HandleFunc()
 }
